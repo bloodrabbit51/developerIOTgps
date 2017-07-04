006585198ebacaf76ef9ba8a35568ff272a5bbc6
rohanraj7316 <rohanraj7316@gmail.com>
2017-07-04 15:43:18 +0530

    updated server and config file

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 /**
  * Created by unio-raj on 4/7/17.
  */
+
+var express = require('express');
+var app = express();
+var router = express.Router();
+var serverconfig = require('./app/config/config.json');
+var devicedataroute = require('./app/routes/device.data.demo');
+
+app.route('/').get(function (req,res) {
+   res.send('hello world');
+});
+
+app.listen(serverconfig.http.port,function () {
+    console.log("http server is running on port number: 8080");
+});
