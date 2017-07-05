a07291c3c0ad7f76405f7117337b3981ca9d0fd3
rohanraj7316 <rohanraj7316@gmail.com>
2017-07-05 13:05:52 +0530

    pulling the latest code

---
+++ app/models/device.data.schema.js
@@ -0,0 +1,61 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+var deviceDataSchema = new Schema({
+    latitude:{
+        type:String,
+        default:''
+    },
+    longitude:{
+        type:String,
+        default:''
+    },
+    speed:{
+        type:String,
+        default:''
+    },
+    angle:{
+        type:String,
+        default:''
+    },
+    altitude:{
+        type:String,
+        default:''
+    },
+    ignition:{
+        type:String,
+        default:''
+    },
+    dtc:{
+        type:String,
+        default:''
+    },
+    engine_rpm:{
+        type:String,
+        default:''
+    },
+    vehicle_speed:{
+        type:String,
+        default:''
+    },
+    fuel:{
+        type:String,
+        default:''
+    },
+    throttleposition:{
+        type:String,
+        default:''
+    },
+    engineload:{
+        type:String,
+        default:''
+    },
+    created:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+var deviceData = mongoose.model('devicedata',deviceDataSchema);
+module.exports = deviceData;
--- server.js
+++ server.js
@@ -8,11 +8,62 @@ var router = express.Router();
 var serverconfig = require('./app/config/config.json');
 var devicedataroute = require('./app/routes/device.data.demo');
 var gpsdevicedataController = require('./app/controllers/device.controllers/data.collector.controller.from.device.js')();
+var mongoose = require('mongoose');
+var deviceData = require('./app/models/device.data.schema.js');
+var morgan = require('morgan');
 
+
+
+mongoose.connect(serverconfig.mongo.db,function (err,res) {
+    if(err){
+        console.log('Error connecting: ',serverconfig.mongo.db,':',err.message);
+    }else {
+        console.log('Succesfully connected to: ',serverconfig.mongo.db);
+    }
+});
+
+app.use(morgan('dev'));
 
 app.route('/').get(function (req,res) {
-   res.send('hello world');
+   var obj = deviceData({
+        name : "Rohan Raj",
+       username : "rohanraj7316@gmail.com",
+       location : "Hyderabad"
+   });
+   obj.save(function (err) {
+      if(err){
+          res.status(400).json({
+              status : "failed",
+              message : "entering data into database failed"
+          });
+      }else{
+          res.status(200).json({
+              status: "success",
+              message: "entring data into database succesfull"
+          });
+      }
+   });
 });
+
+
+
+app.route('/getuserdetail').get(function (req,res) {
+    deviceData.find({},function (err,data) {
+       if(err){
+           res.status(400).json({
+               status:"failed",
+               message:"error while retriving data: ",err
+           });
+       }else{
+           res.status(200).json({
+               status:"success",
+               data: data
+           });
+       }
+    });
+});
+
+
 
 app.listen(serverconfig.http.port,function () {
     console.log("http server is running on port number: 8080");
